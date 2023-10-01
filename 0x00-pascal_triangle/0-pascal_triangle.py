973cd186b9f27abcfc8b43ad7a8d210c86be81c6
Adeniyii <ifedayoadeniyi@gmail.com>
2023-10-01 15:19:49 +0100

    feat: complete impl

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,26 @@
+def pascal_triangle(n):
+    """Implements a simple pascals triangle"""
+    if n <= 0:
+        return []
+
+    i = 1
+    out = [[1]]
+
+    if n >= 2:
+        out.append([1, 1])
+        i += 1
+
+    while i < n:
+        tmp = []
+        curr = out[-1]
+        tmp.append(curr[0])
+
+        for j in range(len(curr) - 1):
+            tmp.append(curr[j] + curr[j+1])
+
+        tmp.append(1)
+        out.append([*tmp])
+        tmp.clear()
+        i += 1
+
+    return out
